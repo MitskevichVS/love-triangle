588737277b0c0cbc28929a89fb6c3778f8e55186
Mitskevich Victor <mv2.mx@yandex.ru>
2019-02-20 14:20:19 +0300

    Task love triangles done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var sum = 0 , index1, index2, index3, total;
+    var newLength = preferences.unshift(0);
+  for (var i = 1; i < newLength; i++) {
+  	index1 = preferences[i];
+  	index2 = preferences[index1];
+  	index3 = preferences[index2];
+  	if (index3 == i){
+  		sum++
+  	}
+  }
+  total = Math.floor(sum/3);
+  return (total);
 };
